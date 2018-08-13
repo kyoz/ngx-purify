2bda3d29cccd4f44e9a1f07c67fcfa8891e7bcea
Kyoz <banminkyoz@gmail.com>
2018-08-13 23:20:03 +0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+// Angular Flex Layout
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Angular Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatOptionModule,
+  MatTooltipModule
+} from '@angular/material';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Angular Flex Layout
+    FlexLayoutModule,
+
+    // Angular Material
+    BrowserAnimationsModule,
+
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
