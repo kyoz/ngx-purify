f87d7571b77a862ce6735ece97fb166b8dab42b2
Kyoz <banminkyoz@gmail.com>
2018-09-07 15:07:13 +0700

    Fullscreen toggle support open/close with diff icon

--- src/app/core/pure-components/pure-toolbar/pure-toolbar.component.ts
+++ src/app/core/pure-components/pure-toolbar/pure-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { PureMenuContainerService } from '../../pure-containers/pure-menu-container/pure-menu-container.service';
 import { PureChatboxContainerService } from '../../pure-containers/pure-chatbox-container/pure-chatbox-container.service';
 import { PureNotificationContainerService } from '../../pure-containers/pure-notification-container/pure-notification-container.service';
@@ -8,30 +8,57 @@ import { PureNotificationContainerService } from '../../pure-containers/pure-not
   templateUrl: './pure-toolbar.component.html',
   styleUrls: ['./pure-toolbar.component.scss']
 })
-export class PureToolbar{
+export class PureToolbar {
+  @HostListener('window:fullscreenchange', ['$event'])
+  @HostListener('window:webkitfullscreenchange', ['$event'])
+  @HostListener('window:mozfullscreenchange', ['$event'])
+  @HostListener('window:msfullscreenchange', ['$event'])
+  onFullScreenChange() {
+    this.isFullScreen = this.isInFullScreen();
+  }
 
   isSearching = false;
+  isFullScreen = false;
 
   constructor(
     public _menuContainer: PureMenuContainerService,
     public _chatboxContainer: PureChatboxContainerService,
     public _notificationContainer: PureNotificationContainerService
-  ) { }
+  ) {}
 
   toggleSearch() {
     this.isSearching = !this.isSearching;
   }
 
-  requestFullscreen() {
-    const demo: any = document.getElementById('pure-admin-dashboard');
-    if (demo.requestFullscreen) {
-      demo.requestFullscreen();
-    } else if (demo.mozRequestFullScreen) { /* Firefox */
-      demo.mozRequestFullScreen();
-    } else if (demo.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-      demo.webkitRequestFullscreen();
-    } else if (demo.msRequestFullscreen) { /* IE/Edge */
-      demo.msRequestFullscreen();
+  toggleFullscreen() {
+    if (!this.isInFullScreen()) {
+      const demo: any = document.getElementById('pure-admin-dashboard');
+      if (demo.requestFullscreen) {
+        demo.requestFullscreen();
+      } else if (demo.mozRequestFullScreen) { /* Firefox */
+        demo.mozRequestFullScreen();
+      } else if (demo.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        demo.webkitRequestFullscreen();
+      } else if (demo.msRequestFullscreen) { /* IE/Edge */
+        demo.msRequestFullscreen();
+      }
+    } else {
+      if (document['exitFullscreen']) {
+        document['exitFullscreen']();
+      } else if (document['mozCancelFullScreen']) { /* Firefox */
+        document['mozCancelFullScreen']();
+      } else if (document['webkitExitFullscreen']) { /* Chrome, Safari and Opera */
+        document['webkitExitFullscreen']();
+      } else if (document['msExitFullscreen']) { /* IE/Edge */
+        document['msExitFullscreen']();
+      }
     }
+  }
+
+  isInFullScreen() {
+    return (document['fullscreenElement'] && document['fullscreenElement'] !== null) ||
+    (document['webkitFullscreenElement'] && document['webkitFullscreenElement'] !== null) ||
+    (document['mozFullScreenElement'] && document['mozFullScreenElement'] !== null) ||
+    (document['msFullscreenElement'] && document['msFullscreenElement'] !== null);
   }
 }
