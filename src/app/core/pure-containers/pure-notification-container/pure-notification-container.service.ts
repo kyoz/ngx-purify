91ef785634cd8a64ea61c1a0ecea5a86ded09ecf
Kyoz <banminkyoz@gmail.com>
2018-09-26 00:49:06 +0700

    Debounce Time to scroll to top for notification container

--- src/app/core/pure-containers/pure-notification-container/pure-notification-container.service.ts
+++ src/app/core/pure-containers/pure-notification-container/pure-notification-container.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable()
 export class PureNotificationContainerService {
   isOpened$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor() {
-    this.isOpened$.subscribe(isOpened => {
+    this.isOpened$.pipe(debounceTime(300), distinctUntilChanged()).subscribe(isOpened => {
       if (!isOpened) {
         // Scroll the notification to top when closed
         const pureNotification = document.getElementsByClassName('notification-wrapper ps ps--active-y');
