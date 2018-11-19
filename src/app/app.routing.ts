b07558c1d246840c570aa56c57553c6e43ad3cd6
Kyoz <banminkyoz@gmail.com>
2018-11-19 21:48:52 +0700

    Update Route correctly for main component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,8 +5,12 @@ import { MainContainer } from './containers/main-container/main-container.compon
 import { MinimalContainer } from './containers/minimal-container/minimal-container.component';
 
 const routes: Routes = [
-  { path: 'app', component: MainContainer, loadChildren: './templates/applications/applications.module#ApplicationsModule' },
-  { path: 'material-components', component: MainContainer, loadChildren: './templates/material-components/material-components.module#MaterialComponentsModule' },
+  { 
+    path: '', component: MainContainer, children: [
+      { path: 'app', loadChildren: './templates/applications/applications.module#ApplicationsModule' },
+      { path: 'material-components', loadChildren: './templates/material-components/material-components.module#MaterialComponentsModule' },
+    ]
+  },
   { path: 'page', component: MinimalContainer, loadChildren: './templates/pages/pages.module#PagesModule' },
   { path: '', redirectTo: 'app', pathMatch: 'full' },
   { path: '**', redirectTo: 'app', pathMatch: 'full' }
