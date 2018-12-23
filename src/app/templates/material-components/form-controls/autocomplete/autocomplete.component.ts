8d71e4f88507d20f8fdcc1e889232dd62a362cc9
Kyoz <banminkyoz@gmail.com>
2018-12-23 22:13:18 +0700

    Update material autocomplete example url

--- src/app/templates/material-components/form-controls/autocomplete/autocomplete.component.ts
+++ src/app/templates/material-components/form-controls/autocomplete/autocomplete.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 // Examples Code
 import * as SIMPLE_AUTOCOMPLETE_CODE from './examples/simple-autocomplete-example/simple-autocomplete-code';
-import * as DISPLAY_VALUE_AUTOCOMPLETE_CODE from './examples/display-value-autocomplete-example/display-value-autocomplete-example-code';
-import * as FILTER_AUTOCOMPLETE_CODE from './examples/filter-autocomplete-example/filter-autocomplete-example-code';
-import * as OPTION_GROUPS_AUTOCOMPLETE_CODE from './examples/option-groups-autocomplete-example/option-groups-autocomplete-example-code';
-import * as FOCUS_FIRST_OPTION_AUTOCOMPLETE_CODE from './examples/focus-first-option-autocomplete-example/focus-first-option-autocomplete-example-code';
+import * as DISPLAY_VALUE_AUTOCOMPLETE_CODE from './examples/display-value-autocomplete-example/display-value-autocomplete-code';
+import * as FILTER_AUTOCOMPLETE_CODE from './examples/filter-autocomplete-example/filter-autocomplete-code';
+import * as OPTION_GROUPS_AUTOCOMPLETE_CODE from './examples/option-groups-autocomplete-example/option-groups-autocomplete-code';
+import * as FOCUS_FIRST_OPTION_AUTOCOMPLETE_CODE from './examples/focus-first-option-autocomplete-example/focus-first-option-autocomplete-code';
 
 @Component({
   selector: 'app-autocomplete',
