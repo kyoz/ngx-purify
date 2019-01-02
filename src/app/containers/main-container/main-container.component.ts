14fbc83050e9ce0dc42543f919bef2c52d17d859
Kyoz <banminkyoz@gmail.com>
2019-01-02 23:34:17 +0700

    Main Container -> Scroll to top when route change

--- src/app/containers/main-container/main-container.component.ts
+++ src/app/containers/main-container/main-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, DoCheck } from '@angular/core';
 import { PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-main-container',
@@ -9,9 +10,15 @@ import { PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
 export class MainContainer implements DoCheck {
   @ViewChild('MAIN_CONTAINER') containerPerfectScrollbar?: PerfectScrollbarDirective;
 
+  constructor(private _router: Router) {
+    this._router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.containerPerfectScrollbar.scrollToTop();
+      }
+  });
+  }
+
   ngDoCheck(): void {
     this.containerPerfectScrollbar.update();
   }
-
-  constructor() { }
 }
