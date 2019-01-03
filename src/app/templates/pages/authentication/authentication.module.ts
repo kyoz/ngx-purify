4fe9af0889493f32e3279342db71b56dbcb41f0a
Kyoz <banminkyoz@gmail.com>
2019-01-04 00:00:16 +0700

    Update login form

--- src/app/templates/pages/authentication/authentication.module.ts
+++ src/app/templates/pages/authentication/authentication.module.ts
@@ -1,6 +1,21 @@
 import { NgModule } from '@angular/core';
+
+// Routing Module
 import { AuthenticationRoutingModule } from './authentication.routing';
+
+// Components
 import { LoginComponent } from './login/login.component';
+
+// Material Modules
+import {
+  MatInputModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule
+} from '@angular/material';
+
+// Other Modules
 import { PureSharedModule } from '../../../shared/shared.module';
 
 @NgModule({
@@ -8,7 +23,17 @@ import { PureSharedModule } from '../../../shared/shared.module';
     LoginComponent
   ],
   imports: [
+    // Material Modules
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+
+    // Routing Module
     AuthenticationRoutingModule,
+
+    // Other Modules
     PureSharedModule
   ]
 })
--- src/app/templates/pages/authentication/login/login.component.ts
+++ src/app/templates/pages/authentication/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginComponent {
+  formCtrl = {
+    email: undefined,
+    password: undefined
   }
 
+  constructor() {
+    this.formCtrl.email = new FormControl('', [Validators.required, Validators.email]);
+    this.formCtrl.password = new FormControl('', [Validators.required]);
+  }
+
+  getEmailErrorMessage() {
+    return this.formCtrl.email.hasError('required') ? 'You must enter an email' :
+        this.formCtrl.email.hasError('email') ? 'This is not a valid email' : '';
+  }
 }
