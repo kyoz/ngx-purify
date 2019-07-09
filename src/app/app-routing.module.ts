04731ec515724d73ba5076f31781935f2663511e
Kyoz <banminkyoz@gmail.com>
2019-07-09 22:56:20 +0700

    Load lazymodule by import()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,36 @@ import { MainContainer } from './containers/main-container/main-container.compon
 import { MinimalContainer } from './containers/minimal-container/minimal-container.component';
 
 const routes: Routes = [
-  { path: 'app', component: MainContainer, loadChildren: './templates/applications/applications.module#ApplicationsModule' },
+  {
+    path: 'app',
+    component: MainContainer,
+    loadChildren: () => import('./templates/applications/applications.module')
+      .then(module => module.ApplicationsModule)
+  },
   {
     path: 'material-components',
     component: MainContainer,
-    loadChildren: './templates/material-components/material-components.module#MaterialComponentsModule'
+    loadChildren: () => import('./templates/material-components/material-components.module')
+      .then(module => module.MaterialComponentsModule)
   },
-  { path: 'pages/errors', component: MainContainer, loadChildren: './templates/pages/errors/errors.module#ErrorsModule' },
-  { path: 'pages', component: MinimalContainer, loadChildren: './templates/pages/pages.module#PagesModule' },
-  { path: 'others', component: MainContainer, loadChildren: './templates/others/others.module#OthersModule' },
+  {
+    path: 'pages/errors',
+    component: MainContainer,
+    loadChildren: () => import('./templates/pages/errors/errors.module')
+      .then(module => module.ErrorsModule)
+  },
+  {
+    path: 'pages',
+    component: MinimalContainer,
+    loadChildren: () => import('./templates/pages/pages.module')
+      .then(module => module.PagesModule)
+  },
+  {
+    path: 'others',
+    component: MainContainer,
+    loadChildren: () => import('./templates/others/others.module')
+      .then(module => module.OthersModule)
+  },
   { path: '', redirectTo: 'app', pathMatch: 'full' },
   { path: '**', redirectTo: 'app', pathMatch: 'full' },
 ];
