327756448d7848309bd07e04bda524ad69887a4d
Kyoz <banminkyoz@gmail.com>
2019-07-09 23:07:58 +0700

    Use paramMap and paramQueryMap for routeWithParam example

--- src/app/templates/others/route-with-params/route-with-params.component.ts
+++ src/app/templates/others/route-with-params/route-with-params.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { combineLatest } from 'rxjs';
 
 @Component({
@@ -12,12 +12,12 @@ export class OthersRouteWithParamsComponent {
 
   constructor(private _activatedRoute: ActivatedRoute) {
     const urlParams = combineLatest(
-      this._activatedRoute.params,
-      this._activatedRoute.queryParams,
+      this._activatedRoute.paramMap,
+      this._activatedRoute.queryParamMap,
       (params, queryParams) => ({ ...params, ...queryParams })
     );
-    urlParams.subscribe((params: any) => {
-      this.params = JSON.stringify(params);
+    urlParams.subscribe((paramMap: ParamMap) => {
+      this.params = JSON.stringify(paramMap);
     });
   }
 }
