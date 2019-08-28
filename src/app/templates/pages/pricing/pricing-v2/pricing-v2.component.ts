e83e93e17c445a5c299942a6281a77f8d22a8477
Kyoz <banminkyoz@gmail.com>
2019-08-28 22:46:10 +0700

    done(template): pricing style 2

--- src/app/templates/pages/pricing/pricing-v2/pricing-v2.component.ts
+++ src/app/templates/pages/pricing/pricing-v2/pricing-v2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PureSettingsService } from '../../../../core/pure-services/pure-settings.service';
 
 @Component({
   selector: 'pricing-v2',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingV2Component implements OnInit {
 
-  constructor() { }
+  constructor(public settings: PureSettingsService) { }
 
   ngOnInit() {
   }
