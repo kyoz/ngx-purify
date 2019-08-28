743213c01887d030f6d035e24d9db9b7feab7b7f
Kyoz <banminkyoz@gmail.com>
2019-08-28 23:06:54 +0700

    use ngIfElse for menu item external link & gen pricing-v3 component

--- src/app/templates/pages/pricing/pricing-v3/pricing-v3.component.ts
+++ src/app/templates/pages/pricing/pricing-v3/pricing-v3.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PureSettingsService } from '../../../../core/pure-services/pure-settings.service';
 
 @Component({
   selector: 'pricing-v3',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricingV3Component implements OnInit {
 
-  constructor() { }
+  constructor(public settings: PureSettingsService) { }
 
   ngOnInit() {
   }
