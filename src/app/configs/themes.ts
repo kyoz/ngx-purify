69954c3227f8c97e88523b7e7262892be9794005
Kyoz <banminkyoz@gmail.com>
2019-09-02 21:54:15 +0700

    update themes

--- src/app/configs/themes.ts
+++ src/app/configs/themes.ts
@@ -1,20 +1,21 @@
 import { IPureTheme } from '../core/pure-interfaces/settings';
 
 export const THEMES: IPureTheme[] = [
-  { 
+  {
     name: 'Default (Light)',
     class: 'pure-default-theme'
   },
   {
-    name: 'Blue (Light)',
-    class: 'pure-blue-light-theme'
+    name: 'Indigo (Light)',
+    class: 'pure-indigo-light-theme'
   },
   {
-    name: 'Purple (Dark)',
-    class: 'pure-purple-dark-theme'
+    name: 'Deep Orange (Dark)',
+    class: 'pure-deep-orange-dark-theme'
   },
   {
     name: 'Pink (Dark)',
     class: 'pure-pink-dark-theme'
   }
 ];
+
